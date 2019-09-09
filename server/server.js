db1f6d0bc635338f72c5dbd996a1ec2064fb678a
javisosa <javi.sosa1617@gmail.com>
2019-09-08 17:13:56 -0700

    creates form, sets up express server, adds check-in route

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static('public'));
+
+
+app.get("/jsmember", function (req, res) {
+    res.json(memberlist);
+  });
+
+app.post("/jscheckin", (req, res) => {
+    const member={
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
+        location: "",
+    }
+    res.status(200).send(req.body);
+    });
+
+  app.get("*", function (req, res) {
+    res.status(404).send("error: page not found");
+  });
+  
+  module.exports = app;
