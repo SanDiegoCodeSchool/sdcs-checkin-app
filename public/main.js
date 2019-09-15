a3450b3f5603cecb18a80be0db9d81c846c43c71
javisosa <javi.sosa1617@gmail.com>
2019-09-15 16:54:52 -0700

    sent data from front end to back end

--- public/main.js
+++ public/main.js
@@ -1,25 +1,25 @@
-function handleCheckin(e){
-    console.log('Form was submitted.');
-    e.preventDefault();
-    const url = '/jscheckin';
-    const data = {
-        email: document.getElementById('email'),
-        firstName: document.getElementById('first-name'),
-        lastName: document.getElementById('last-name'),
-        phoneNumber: document.getElementById('phone-number'),
-    };
-    console.log(`about to send ${JSON.stringify(data)}`);
-    fetch(url, {
-      method: 'POST',
-      body: data,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+function handleCheckin(e) {
+  console.log('Form was submitted.');
+  // e.preventDefault();
+  const url = '/jscheckin';
+  const data = {
+    email: document.getElementById('email').value,
+    firstName: document.getElementById('first-name').value,
+    lastName: document.getElementById('last-name').value,
+    phoneNumber: document.getElementById('phone-number').value,
+  };
+  console.log(`about to send ${JSON.stringify(data)}`);
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(res => window.location.replace("confirmation.html"))
     .catch(error => {
       console.error('Error:', error);
     });
-  
-  }
+
+}
--- server/server.js
+++ server/server.js
@@ -4,27 +4,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const checkedIn = [];
+
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(express.static('public'));
 
-app.get("/jsmember", function (req, res) {
-    res.json(memberlist);
-});
-
 app.post("/jscheckin", (req, res) => {
-  const member={
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phoneNumber: req.body.phoneNumber,
-      location: "",
+  const attendee = {
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phoneNumber: req.body.phoneNumber,
+    location: "",
   }
+  checkedIn.push(attendee);
+  console.log(checkedIn);
   res.status(200).send(req.body);
 });
 
 app.get("*", function (req, res) {
   res.status(404).send("error: page not found");
 });
-  
-  module.exports = app;
+
+module.exports = app;
