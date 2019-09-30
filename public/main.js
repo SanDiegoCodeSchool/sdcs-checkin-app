7596b63e5a45876ba35942d7bace151a0699fe0f
Roy Hobbs <roy.hobbs@sdcs.io>
2019-09-29 17:18:24 -0700

    additions from group

--- public/main.js
+++ public/main.js
@@ -1,23 +1,39 @@
 function handleCheckin(e) {
   const attendeeInfo = {
-    email: document.getElementById('email').value,
-    firstName: document.getElementById('first-name').value,
-    lastName: document.getElementById('last-name').value,
-    phoneNumber: document.getElementById('phone-number').value,
+    email: document.getElementById("email").value,
+    firstName: document.getElementById("first-name").value,
+    lastName: document.getElementById("last-name").value,
+    phoneNumber: document.getElementById("phone-number").value
   };
 
-  fetch('/jscheckin', {
-    method: 'POST',
+  // let total=document.getElementById('total-checkins')
+
+  fetch("/api/Attendees", {
+    method: "POST",
     body: JSON.stringify(attendeeInfo),
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     }
   })
     .then(res => res.json())
     .then(res => window.location.replace("confirmation.html"))
     .catch(error => {
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
-
 }
 
+// fetch("/api/Attendees", {
+//   method: "GET",
+//   body: JSON.stringify(attendeeInfo),
+//   headers: {
+//     "Content-Type": "application/json"
+//   }
+// })
+
+  // .then(response => response.json())
+  // .then(data => {
+  //   for (const users of data.events) {
+  //     let total =data.events.length;
+  //   }
+  //   console.log(total,users);
+  // });
